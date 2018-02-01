f799022c9a32392ca72a48d9f44700e61cdaf080
wakewakame <0214.taiki@gmail.com>
2018-02-01 13:56:32 +0900

    a

--- functions/math/sigmoid.glsl
+++ functions/math/sigmoid.glsl
@@ -1,11 +1,17 @@
 /*
 *ドキュメント
 
-float sigmoid(float x, float a)
-	シグモイド関数
+float sigmoid1(float x, float a)
+	原点で点対称なシグモイド関数
 	xの範囲は-1.0, 1.0
 	aはx=0のときの関数の傾き
-	戻り値の範囲は0.0, 1.0
+	戻り値の範囲は-1.0, 1.0
+
+float sigmoid2(float x, float a)
+	sigmoid1関数の(x, 戻り値)が(-1.0, -1.0), (1.0, 1.0)を通るように拡大縮小した関数
+	xの範囲は-1.0, 1.0
+	戻り値の範囲は-1.0, 1.0
+	a=0.0のときはxをそのまま返す
 */
 
 #ifdef GL_ES
@@ -18,14 +24,21 @@ uniform float time;
 uniform vec2 mouse;
 uniform vec2 resolution;
 
-float sigmoid(float x, float a){
-	return 1.0 / (1.0 + exp(-4.0 * a * x));
+float sigmoid1(float x, float a){
+	float ex = exp(-2.0 * a * x);
+	return (1.0 - ex) / (1.0 + ex);
+}
+
+float sigmoid2(float x, float a){
+	float d = sigmoid1(1.0, a);
+	if (d != 0.0) return sigmoid1(x, a) / d;
+	else return x;
 }
 
 void main(void){
 	vec2 p = gl_FragCoord.xy / resolution;
 	float x = (p.x * 2.0) - 1.0;
 	float a = mouse.x * 100.0;
-	float n = sigmoid(x, a);
+	float n = sigmoid1(x, a);
 	gl_FragColor = vec4(vec3(n), 1.0);
 }
